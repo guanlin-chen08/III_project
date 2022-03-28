4997528436bb17ceba16bfdcfccc62543b813d9a
guanlin-chen08 <m10902147@gapps.ntust.edu.tw>
2022-03-28 20:18:30 +0800

    first

---
+++ III_View/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IiiViewConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'III_View'
---
+++ III_View/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+
+
+# Create your views here.
+def test(request):
+    return render(request, 'main.html')
